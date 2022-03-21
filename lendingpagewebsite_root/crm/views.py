be40f79f06e8415cd6f2448b0b679bb7dd4cd10e
Storm039 <alexandr.bogaychuk@gmail.com>
2022-03-21 15:19:06 +0200

    create_price_crm_css

---
+++ lendingpagewebsite_root/crm/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from .models import Order
+from cms.models import CmsSlider
+from price.models import PriceCard, PriceTable
+
+
+# Create your views here.
+def first_page(request):
+    slider_list = CmsSlider.objects.all()
+    pc_1 = PriceCard.objects.get(pk=1)
+    pc_2 = PriceCard.objects.get(pk=2)
+    pc_3 = PriceCard.objects.get(pk=3)
+    price_table = PriceTable.objects.all()
+    dict_obj = {"slider_list": slider_list,
+                'pc_1': pc_1,
+                'pc_2': pc_2,
+                'pc_3': pc_3,
+                'price_table': price_table,
+                }
+    return render(request, "./index.html", dict_obj)
+
+
+def thanks_page(request):
+    name = request.POST["name"]
+    phone = request.POST["phone"]
+    element = Order(order_name=name, order_phone=phone)
+    element.save()
+    return render(request, "./thanks_page.html", {'name': name,
+                                                  'phone': phone})
